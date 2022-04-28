ffe1925a6bdb0d560e0e9bc0f2733021ec5fe5a8
Kellbber <kellbberbarcarolo@gmail.com>
2022-04-28 20:09:32 -0300

    refactor - ajustado para  Atlas e deploy Render

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
-const connectionToDatabase = () => {
+const connectToDatabase = () => {
   mongoose
-    .connect('mongodb://localhost:27017/xis-db', {
+    .connect(process.env.URI_DATABASE, { 
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log('MONGODB CONNECT!'))
-    .catch((err) => console.log(`Erro ao conectar ao MongoDB, erro: ${err}`));
+    .then(() => console.log('MongoDB Atlas Conectado!'))
+    .catch((error) =>
+      console.log(`Erro ao conectar com o MongoDB, erro: ${error}`),
+    );
 };
 
-
-module.exports = connectionToDatabase;
+module.exports = connectToDatabase;
--- src/routes/xis.route.js
+++ src/routes/xis.route.js
@@ -1,14 +1,28 @@
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocument = require('../../swagger.json');
+
 const express = require('express');
 const router = express.Router();
 const controllerXis = require('../controllers/xis.controller');
-const {validId, validObjectBody} = require("../middlewares/xis.middleware");
+const { validId, validObjectBody } = require('../middlewares/xis.middleware');
 
 
-router.get('/all-xis', controllerXis.findAllXisController)
+//rotas para o swagger
+router.use("/api-docs", swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument))
+
+
+
+router.get('/all-xis', controllerXis.findAllXisController);
 router.get('/one-xis/:id', validId, controllerXis.findByIdXisController);
 router.post('/create-xis', validObjectBody, controllerXis.createXisController);
-router.put('/update-xis/:id',validId, validObjectBody, controllerXis.updateXisController);
+router.put(
+  '/update-xis/:id',
+  validId,
+  validObjectBody,
+  controllerXis.updateXisController,
+);
 router.delete('/delete-xis/:id', validId, controllerXis.deleteXisController);
-
 
 module.exports = router;
